811208cf58f184413d709216612618b5347b3cb3
VishwasChahal <collegersblog@gmail.com>
2024-06-11 04:08:51 +0530

    yes

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true, // Enable continuous loop mode
+        autoplay: {
+            delay: 5000, // Adjust autoplay delay as needed
+        },
+        pagination: {
+            el: '.swiper-pagination1',
+            clickable: true,
+        }
+    });
+
+    var swiper2 = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true, // Enable continuous loop mode
+        autoplay: {
+            delay: 5000, // Adjust autoplay delay as needed
+        },
+        pagination: {
+            el: '.swiper-pagination2',
+            clickable: true,
+        }
+    });
+
+    var swiper3 = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true, // Enable continuous loop mode
+        autoplay: {
+            delay: 5000, // Adjust autoplay delay as needed
+        },
+        pagination: {
+            el: '.swiper-pagination3',
+            clickable: true,
+        }
+    });
+});
