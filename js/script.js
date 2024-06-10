ed3ece574557ec66c04d4116dc68c5ad27268fa9
VishwasChahal <collegersblog@gmail.com>
2024-06-11 04:43:17 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -35,3 +35,22 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+    });
+});
+
